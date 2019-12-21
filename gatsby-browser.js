f1683138081fd5883a8a02e473e3cf2e754ebbb9
Ulhas Mandrawadkar <ulhas.sm@gmail.com>
2019-12-21 22:52:32 +0530

    Adds scss modules; renames folders

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,4 +5,4 @@
  */
 
 // You can delete this file if you're not using it
-import "./src/settings/styles.scss"
+import "./src/styles/global.scss"
--- src/utils/typography.tsx
+++ src/utils/typography.tsx
@@ -1,4 +1,5 @@
 import Typography from "typography"
+import funstonTheme from "typography-theme-funston"
 
 const typography = new Typography({
   baseFontSize: "18px",
