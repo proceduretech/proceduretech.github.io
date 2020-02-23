fe10ab6bef1a6ad55f57ea1afe4023a6f877ec06
Ulhas Mandrawadkar <ulhas.sm@gmail.com>
2020-02-23 11:36:54 +0530

    Adds className to DarkBackground

--- src/components/shared/shared.tsx
+++ src/components/shared/shared.tsx
@@ -1,9 +1,17 @@
 import React from "react"
 
 interface DarkBackgroundProps {
+  className?: string
   children?: React.ReactNode
 }
 
-export const DarkBackground = ({ children }: DarkBackgroundProps) => {
-  return <div className="bg-background-dark">{children}</div>
+export const DarkBackground = ({
+  className,
+  children,
+}: DarkBackgroundProps) => {
+  return (
+    <div className={`bg-background-dark ${className ? className : ""}`}>
+      {children}
+    </div>
+  )
 }
